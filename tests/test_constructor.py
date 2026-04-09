ac9a397c4b30fe20ca832f0d1c452c86ca063145
Stas Basov <stasbasov@yandex.com>
2026-04-09 22:24:47 +0500

    Full working tests

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,26 +1,23 @@
 from selenium.webdriver.support import expected_conditions as EC
 from locators import MainPageLocators
 
-#1
 class TestConstructor:
-    
     def test_switch_to_sauces_tab(self, driver, wait):
         wait.until(EC.visibility_of_element_located(MainPageLocators.BUNS_TAB))
         wait.until(EC.element_to_be_clickable(MainPageLocators.SAUCES_TAB)).click()
         active_tab = wait.until(EC.visibility_of_element_located(MainPageLocators.ACTIVE_TAB))
         assert "Соусы" in active_tab.text or "Соус" in active_tab.text
-    
+
     def test_switch_to_fillings_tab(self, driver, wait):
         wait.until(EC.visibility_of_element_located(MainPageLocators.BUNS_TAB))
         wait.until(EC.element_to_be_clickable(MainPageLocators.FILLINGS_TAB)).click()
         active_tab = wait.until(EC.visibility_of_element_located(MainPageLocators.ACTIVE_TAB))
         assert "Начинки" in active_tab.text or "Начинка" in active_tab.text
-    
+
     def test_switch_to_buns_tab(self, driver, wait):
         wait.until(EC.visibility_of_element_located(MainPageLocators.BUNS_TAB))
         wait.until(EC.element_to_be_clickable(MainPageLocators.SAUCES_TAB)).click()
         wait.until(EC.element_to_be_clickable(MainPageLocators.FILLINGS_TAB)).click()
         wait.until(EC.element_to_be_clickable(MainPageLocators.BUNS_TAB)).click()
         active_tab = wait.until(EC.visibility_of_element_located(MainPageLocators.ACTIVE_TAB))
-        assert "Булки" in active_tab.text or "Булка" in active_tab.text"# develop branch" 
-"# " 
+        assert "Булки" in active_tab.text or "Булка" in active_tab.text
--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -1,15 +1,13 @@
 from selenium.webdriver.support import expected_conditions as EC
 from locators import MainPageLocators, AuthPageLocators, AccountPageLocators
 from data_generators import generate_unique_email, generate_password, generate_name
-#1
 
 class TestPersonalAccount:
-    
     def setup_method(self):
         self.test_email = generate_unique_email()
         self.test_password = generate_password()
         self.test_name = generate_name()
-    
+
     def register_and_login(self, driver, wait):
         wait.until(EC.element_to_be_clickable(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)).click()
         wait.until(EC.element_to_be_clickable(AuthPageLocators.REGISTER_LINK)).click()
@@ -17,43 +15,33 @@ class TestPersonalAccount:
         driver.find_element(*AuthPageLocators.EMAIL_INPUT).send_keys(self.test_email)
         driver.find_element(*AuthPageLocators.PASSWORD_INPUT).send_keys(self.test_password)
         driver.find_element(*AuthPageLocators.REGISTER_BUTTON).click()
-        
         wait.until(EC.visibility_of_element_located(AuthPageLocators.LOGIN_BUTTON))
-        
         driver.get("https://stellarburgers.education-services.ru/")
         wait.until(EC.element_to_be_clickable(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)).click()
         wait.until(EC.visibility_of_element_located(AuthPageLocators.EMAIL_INPUT)).send_keys(self.test_email)
         driver.find_element(*AuthPageLocators.PASSWORD_INPUT).send_keys(self.test_password)
         driver.find_element(*AuthPageLocators.LOGIN_BUTTON).click()
         wait.until(EC.visibility_of_element_located(MainPageLocators.ORDER_BUTTON))
-    
+
     def test_go_to_personal_account(self, driver, wait):
         self.register_and_login(driver, wait)
         wait.until(EC.element_to_be_clickable(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)).click()
-        profile_link = wait.until(EC.visibility_of_element_located(AccountPageLocators.PROFILE_LINK))
-        assert profile_link.is_displayed()
-    
+        assert wait.until(EC.visibility_of_element_located(AccountPageLocators.PROFILE_LINK)).is_displayed()
+
     def test_logout_from_account(self, driver, wait):
         self.register_and_login(driver, wait)
         wait.until(EC.element_to_be_clickable(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)).click()
-        wait.until(EC.visibility_of_element_located(AccountPageLocators.PROFILE_LINK))
         wait.until(EC.element_to_be_clickable(AccountPageLocators.LOGOUT_BUTTON)).click()
-        login_button = wait.until(EC.visibility_of_element_located(AuthPageLocators.LOGIN_BUTTON))
-        assert login_button.is_displayed()
-    
+        assert wait.until(EC.visibility_of_element_located(AuthPageLocators.LOGIN_BUTTON)).is_displayed()
+
     def test_go_to_constructor_from_personal_account(self, driver, wait):
         self.register_and_login(driver, wait)
         wait.until(EC.element_to_be_clickable(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)).click()
-        wait.until(EC.visibility_of_element_located(AccountPageLocators.PROFILE_LINK))
         wait.until(EC.element_to_be_clickable(MainPageLocators.CONSTRUCTOR_BUTTON)).click()
-        order_button = wait.until(EC.visibility_of_element_located(MainPageLocators.ORDER_BUTTON))
-        assert order_button.is_displayed()
-    
+        assert wait.until(EC.visibility_of_element_located(MainPageLocators.ORDER_BUTTON)).is_displayed()
+
     def test_go_to_constructor_by_logo(self, driver, wait):
         self.register_and_login(driver, wait)
         wait.until(EC.element_to_be_clickable(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)).click()
-        wait.until(EC.visibility_of_element_located(AccountPageLocators.PROFILE_LINK))
         wait.until(EC.element_to_be_clickable(MainPageLocators.LOGO)).click()
-        order_button = wait.until(EC.visibility_of_element_located(MainPageLocators.ORDER_BUTTON))
-        assert order_button.is_displayed()"# develop branch" 
-"# " 
+        assert wait.until(EC.visibility_of_element_located(MainPageLocators.ORDER_BUTTON)).is_displayed()
